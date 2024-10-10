f4cc94680effacba7872c736dc0050c9dccce42b
Jakub Kohoutek <jkohoutek@microsoft.com>
2024-10-10 12:38:16 +0200

    Improve extraction routine

--- coffee_machine_scale.ino
+++ coffee_machine_scale.ino
@@ -23,6 +23,10 @@
 #define DOUBLE_DOSE_ADDRESS  4
 #define STATISTICS_ADDRESS   8
 #define LAST_TIME_ADDRESS    12
+#define SINGLE_NAKED_ADDRESS 16
+#define DOUBLE_NAKED_ADDRESS 20
+#define SINGLE_SPOUT_ADDRESS 24
+#define DOUBLE_SPOUT_ADDRESS 28
 
 #define OLED         U8G2_SH1106_128X64_NONAME_F_HW_I2C
 #define SMALL_FONT   u8g2_font_8x13B_tf // XE font group
@@ -35,12 +39,19 @@ enum MenuItem {
   Scale
 };
 
+enum Portafilter {
+  BottomLess,
+  Spouted
+};
+
 enum Screen {
   MenuScreen,
   SettingsScreen,
   Timer,
   InfoScreen,
-  ScaleScreen
+  ScaleScreen,
+  PortafilterScreen,
+  WeightCoeffScreen
 };
 
 // HX711 pins definition
@@ -59,64 +70,30 @@ const char*    ssid                     = STASSID;
 const char*    password                 = STAPSK;
 const char*    deviceId                 = "Coffee machine scale";
 
-long           encoderPosition    = 0;
-unsigned long  singleShotLimit    = 0;
-unsigned long  doubleShotLimit    = 0;
-unsigned long  grindedDosesCount  = 0;
-unsigned long  lastShotTime       = 0;
-unsigned long  lastActivityMillis = 0;
-Screen         currentScreen      = MenuScreen;
-MenuItem       menu[]             = { Scale, DoubleDose, SingleDose, Info };
-MenuItem*      selectedItem       = menu;
-short          menuLength         = sizeof(menu) / sizeof(*menu);
-bool           ignoreNextPush     = false;
+long           encoderPosition       = 0;
+unsigned long  singleShotLimit       = 0;
+unsigned long  doubleShotLimit       = 0;
+unsigned long  singleSpoutedCoeff    = 0;
+unsigned long  singleNakedCoeff      = 0;
+unsigned long  doubleSpoutedCoeff    = 0;
+unsigned long  doubleNakedCoeff      = 0;
+unsigned long  grindedDosesCount     = 0;
+unsigned long  lastShotTime          = 0;
+unsigned long  lastActivityMillis    = 0;
+Screen         currentScreen         = MenuScreen;
+MenuItem       mainMenu[]                = { Scale, DoubleDose, SingleDose, Info };
+MenuItem*      selectedItem          = mainMenu;
+Portafilter    portafilterMenu[]     = { Spouted, BottomLess };
+Portafilter    selectedPortafilter   = Spouted;
+short          menuLength            = sizeof(mainMenu) / sizeof(*mainMenu);
+short          portafilterMenuLength = sizeof(portafilterMenu) / sizeof(*portafilterMenu);
+bool           ignoreNextPush        = false;
 
 AsyncWebServer server(80);
 OLED           display(U8G2_R0, U8X8_PIN_NONE);
 Button         pushButton(ENCODER_BUTTON);
 HX711          loadcellA;
 HX711          loadcellB;
-
-void drawMessage(const char *message) {
-  display.firstPage();
-  do {
-    display.setFont(SMALL_FONT);
-    unsigned short stringWidth = display.getStrWidth(message);
-    display.drawStr(64 - stringWidth / 2, 60, message);
-    display.drawXBMP(64 - scaleIconBitmapWidth / 2, 0, scaleIconBitmapWidth, scaleIconBitmapHeight, scaleIconBitmap);
-  } while (display.nextPage());
-}
-
-void showGrams(unsigned long milligrams, const char *title) {
-  unsigned int grams = milligrams / 1000;
-  byte gramDecimals = (milligrams % 1000) / 100;
-  String gramsString = String(grams) + "." + String(gramDecimals);
-
-  display.firstPage();
-  do {
-    display.setFont(SMALL_FONT);
-    display.drawStr(0, 15, title);
-    display.setFont(LARGE_FONT);
-    unsigned short stringWidth = display.getStrWidth(grams < 10 ? "0:0" : "10:0");
-    display.drawStr(64 - stringWidth / 2, 55, gramsString.c_str());
-  } while (display.nextPage());
-}
-
-void showWeight(float loadCellWeight, const char *title) {
-  if (abs(loadCellWeight) < 0.1) {
-    loadCellWeight = 0;
-  }
-
-  display.firstPage();
-  do {
-    display.setFont(SMALL_FONT);
-    display.drawStr(0, 15, title);
-    display.setFont(LARGE_FONT);
-    String weightString = String(loadCellWeight, 1);
-    unsigned short stringWidth = display.getStrWidth(weightString.c_str());
-    display.drawStr(64 - stringWidth / 2, 55, weightString.c_str());
-  } while (display.nextPage());
-}
 
 const char *getMenuItemString(MenuItem menuItem) {
   switch (menuItem) {
@@ -131,17 +108,12 @@ const char *getMenuItemString(MenuItem menuItem) {
   }
 }
 
-void showInfo() {
-  currentScreen = InfoScreen;
-  String shotMadeString = String(grindedDosesCount);
-  display.firstPage();
-  do {
-    display.setFont(SMALL_FONT);
-    display.drawStr(0, 15, (String("Shots: ") + String(grindedDosesCount)).c_str());
-    display.drawStr(0, 30, (String("Last time: ") + String(float(lastShotTime) / 1000, 1)).c_str());
-    display.drawStr(0, 45, (String("IP: ") + WiFi.localIP().toString()).c_str());
-    display.drawStr(0, 60, (String("Net: ") + WiFi.SSID()).c_str());
-  } while (display.nextPage());
+unsigned long *getCoeffPtr() {
+  if (*selectedItem == SingleDose) {
+    return selectedPortafilter == BottomLess ? &singleNakedCoeff : &singleSpoutedCoeff;
+  }
+  // else it is double dose
+  return selectedPortafilter == BottomLess ? &doubleNakedCoeff : &doubleSpoutedCoeff;
 }
 
 float getTotalWeight() {
@@ -198,18 +170,111 @@ void showScale(const char *message) {
   };
 }
 
-void showMenu() {
+/*
+  Display functions - responsible for rendering views on the screen
+*/
+
+void showGramsWithTitle(unsigned long milligrams, const char *title) {
+  unsigned int grams = milligrams / 1000;
+  byte gramDecimals = (milligrams % 1000) / 100;
+  String gramsString = String(grams) + "." + String(gramDecimals);
+
+  display.firstPage();
+  do {
+    display.setFont(SMALL_FONT);
+    display.drawStr(0, 15, title);
+    display.setFont(LARGE_FONT);
+    unsigned short stringWidth = display.getStrWidth(grams < 10 ? "0:0" : "10:0");
+    display.drawStr(64 - stringWidth / 2, 55, gramsString.c_str());
+  } while (display.nextPage());
+}
+
+void showWelcomeScreen() {
+  display.firstPage();
+  do {
+    display.drawXBMP(
+      64 - coffeeIconBitmapWidth / 2,
+      32 - coffeeIconBitmapHeight / 2,
+      coffeeIconBitmapWidth,
+      coffeeIconBitmapHeight,
+      coffeeIconBitmap
+    );
+  } while (display.nextPage());
+}
+
+void showTaringScreen() {
+  const char * message = "Taring...";
+  display.firstPage();
+  do {
+    display.setFont(SMALL_FONT);
+    unsigned short stringWidth = display.getStrWidth(message);
+    display.drawStr(64 - stringWidth / 2, 60, message);
+    display.drawXBMP(64 - scaleIconBitmapWidth / 2, 0, scaleIconBitmapWidth, scaleIconBitmapHeight, scaleIconBitmap);
+  } while (display.nextPage());
+}
+
+void showMainMenu() {
   currentScreen = MenuScreen;
+
   display.firstPage();
   do {
     display.setFont(SMALL_FONT);
     for (short i = 0; i < menuLength; i++) {
-      String selectionIndicator = *selectedItem == menu[i] ? "> " : "  ";
-      String lineText = selectionIndicator + getMenuItemString(menu[i]);
+      String selectionIndicator = *selectedItem == mainMenu[i] ? "> " : "  ";
+      String lineText = selectionIndicator + getMenuItemString(mainMenu[i]);
       display.drawStr(0, 15 * (i + 1), lineText.c_str());
     }
   } while (display.nextPage());
 }
+
+void showPortafilterMenu() {
+  currentScreen = PortafilterScreen;
+
+  display.firstPage();
+  do {
+    display.setFont(SMALL_FONT);
+    display.drawStr(0, 15, "Portafilter:");
+    for (short i = 0; i < portafilterMenuLength; i++) {
+      String selectionIndicator = selectedPortafilter == portafilterMenu[i] ? "> " : "  ";
+      String lineText = selectionIndicator + (portafilterMenu[i] == BottomLess ? "Bottomless" : "Spouted");
+      display.drawStr(0, 15 * (i + 2), lineText.c_str());
+    }
+  } while (display.nextPage());
+}
+
+void showWeight(float loadCellWeight, const char *title) {
+  if (abs(loadCellWeight) < 0.1) {
+    loadCellWeight = 0;
+  }
+
+  display.firstPage();
+  do {
+    display.setFont(SMALL_FONT);
+    display.drawStr(0, 15, title);
+    display.setFont(LARGE_FONT);
+    String weightString = String(loadCellWeight, 1);
+    unsigned short stringWidth = display.getStrWidth(weightString.c_str());
+    display.drawStr(64 - stringWidth / 2, 55, weightString.c_str());
+  } while (display.nextPage());
+}
+
+void showInfo() {
+  currentScreen = InfoScreen;
+  String shotMadeString = String(grindedDosesCount);
+
+  display.firstPage();
+  do {
+    display.setFont(SMALL_FONT);
+    display.drawStr(0, 15, (String("Shots: ") + String(grindedDosesCount)).c_str());
+    display.drawStr(0, 30, (String("Last time: ") + String(float(lastShotTime) / 1000, 1)).c_str());
+    display.drawStr(0, 45, (String("IP: ") + WiFi.localIP().toString()).c_str());
+    display.drawStr(0, 60, (String("Net: ") + WiFi.SSID()).c_str());
+  } while (display.nextPage());
+}
+
+/*
+  Helper functions
+*/
 
 void turnOnWiFi() {
   WiFi.mode(WIFI_STA);
@@ -250,16 +315,7 @@ void setup() {
   display.begin();
   display.setBusClock(BUS_CLOCK_SPEED);
 
-  display.firstPage();
-  do {
-    display.drawXBMP(
-      64 - coffeeIconBitmapWidth / 2,
-      32 - coffeeIconBitmapHeight / 2,
-      coffeeIconBitmapWidth,
-      coffeeIconBitmapHeight,
-      coffeeIconBitmap
-    );
-  } while (display.nextPage());
+  showWelcomeScreen();
 
   // Start WiFi
   turnOnWiFi();
@@ -274,6 +330,15 @@ void setup() {
   doubleShotLimit = readFromMemory(DOUBLE_DOSE_ADDRESS);
   grindedDosesCount = readFromMemory(STATISTICS_ADDRESS);
   lastShotTime = readFromMemory(LAST_TIME_ADDRESS);
+
+  // writeToMemory(SINGLE_SPOUT_ADDRESS, 0);
+  // writeToMemory(SINGLE_NAKED_ADDRESS, 0);
+  // writeToMemory(DOUBLE_SPOUT_ADDRESS, 0);
+  // writeToMemory(DOUBLE_NAKED_ADDRESS, 0);
+  singleSpoutedCoeff = readFromMemory(SINGLE_SPOUT_ADDRESS);;
+  singleNakedCoeff   = readFromMemory(SINGLE_NAKED_ADDRESS);;
+  doubleSpoutedCoeff = readFromMemory(DOUBLE_SPOUT_ADDRESS);;
+  doubleNakedCoeff   = readFromMemory(DOUBLE_NAKED_ADDRESS);;
 
   // Initiate over the air programming
   OTA::initialize(deviceId);
@@ -297,7 +362,7 @@ void setup() {
 
   // Start the server
   server.begin();
-  showMenu();
+  showMainMenu();
 }
 
 void handleWebSerialMessage(uint8_t *data, size_t len) {
@@ -333,32 +398,72 @@ void changeTargetWeight(unsigned long *targetWeightPtr, bool up, const char *tit
   } else if (*targetWeightPtr >= 100) {
     *targetWeightPtr -= 100;
   }
-  showGrams(*targetWeightPtr, title);
+  showGramsWithTitle(*targetWeightPtr, title);
+}
+
+void changeWeightCoefficient(unsigned long *weightCoeffPtr, bool up, const char *title) {
+  if (up) {
+    *weightCoeffPtr += 100;
+  } else if (*weightCoeffPtr >= 100) {
+    *weightCoeffPtr -= 100;
+  }
+  showGramsWithTitle(*weightCoeffPtr, title);
+}
+
+void writeWeightCoefficientToMemory() {
+  unsigned long newWeightCoefficient = *getCoeffPtr();
+  if (*selectedItem == SingleDose && selectedPortafilter == BottomLess) {
+    writeToMemory(SINGLE_NAKED_ADDRESS, newWeightCoefficient);
+  }
+
+  if (*selectedItem == SingleDose && selectedPortafilter == Spouted) {
+    writeToMemory(SINGLE_SPOUT_ADDRESS, newWeightCoefficient);
+  }
+
+  if (*selectedItem == DoubleDose && selectedPortafilter == BottomLess) {
+    writeToMemory(DOUBLE_NAKED_ADDRESS, newWeightCoefficient);
+  }
+
+  if (*selectedItem == DoubleDose && selectedPortafilter == Spouted) {
+    writeToMemory(DOUBLE_SPOUT_ADDRESS, newWeightCoefficient);
+  }
 }
 
 void handleRotation(bool clockwise) {
   switch (currentScreen) {
     case MenuScreen:
       if (clockwise) {
-        if (*selectedItem == menu[menuLength - 1]) {
-          selectedItem = menu;
+        if (*selectedItem == mainMenu[menuLength - 1]) {
+          selectedItem = mainMenu;
         } else {
           selectedItem++;
         }
       } else {
-        if (*selectedItem == menu[0]) {
-          selectedItem = &menu[menuLength - 1];
+        if (*selectedItem == mainMenu[0]) {
+          selectedItem = &mainMenu[menuLength - 1];
         } else {
           selectedItem--;
         }
       }
-      showMenu();
+      showMainMenu();
       break;
     case SettingsScreen:
       changeTargetWeight(
         (*selectedItem == SingleDose) ? &singleShotLimit : &doubleShotLimit,
         clockwise,
         getMenuItemString(*selectedItem));
+      break;
+    case PortafilterScreen:
+      {
+        selectedPortafilter = selectedPortafilter == BottomLess ? Spouted : BottomLess;
+        showPortafilterMenu();
+        break;
+      }
+    case WeightCoeffScreen:
+      changeWeightCoefficient(
+        getCoeffPtr(),
+        clockwise,
+        selectedPortafilter == BottomLess ? "Bottomless" : "Spouted");
       break;
   }
 }
@@ -373,7 +478,7 @@ void handleButtonPush() {
     case MenuScreen:
       {
         if (*selectedItem == SingleDose || *selectedItem == DoubleDose) {
-          startExtraction(*selectedItem);
+          showPortafilterMenu();
           break;
         }
         if (*selectedItem == Info) {
@@ -381,16 +486,21 @@ void handleButtonPush() {
           break;
         }
         if (*selectedItem == Scale) {
-          drawMessage("Taring...");
+          showTaringScreen();
           loadcellA.tare();
           loadcellB.tare();
           showScale("Weight (g):");
           break;
         }
       }
+    case PortafilterScreen:
+      {
+        startExtraction();
+        break;
+      }
     case InfoScreen:
       {
-        showMenu();
+        showMainMenu();
         break;
       }
     case SettingsScreen:
@@ -399,12 +509,18 @@ void handleButtonPush() {
         writeToMemory(
           singleDose ? SINGLE_DOSE_ADDRESS : DOUBLE_DOSE_ADDRESS,
           singleDose ? singleShotLimit : doubleShotLimit);
-        showMenu();
+        showMainMenu();
+        break;
+      }
+    case WeightCoeffScreen:
+      {
+        writeWeightCoefficientToMemory();
+        showPortafilterMenu();
         break;
       }
     case ScaleScreen:
       {
-        showMenu();
+        showMainMenu();
         break;
       }
   }
@@ -415,11 +531,18 @@ void handleLongButtonPush() {
     case MenuScreen:
       if (*selectedItem == SingleDose || *selectedItem == DoubleDose) {
         currentScreen = SettingsScreen;
-        showGrams(*selectedItem == SingleDose ? singleShotLimit : doubleShotLimit, getMenuItemString(*selectedItem));
+        showGramsWithTitle(*selectedItem == SingleDose ? singleShotLimit : doubleShotLimit, getMenuItemString(*selectedItem));
         break;
       }
       if (*selectedItem == Info) {
         // Reset?
+        break;
+      }
+    case PortafilterScreen:
+      {
+        currentScreen = WeightCoeffScreen;
+        unsigned long * weightCoeffPtr = getCoeffPtr();
+        showGramsWithTitle(*weightCoeffPtr, selectedPortafilter == BottomLess ? "Bottomless" : "Spouted");
         break;
       }
     default:
@@ -454,37 +577,47 @@ void loop() {
   //    }
 }
 
-void startExtraction(MenuItem selectedItem) {
+void startExtraction() {
   currentScreen = Timer;
 
   float finalTime = 0.0f;
   float timeLimit = 60.0f;  // Pump shouldn't run more than 60 senconds without rest
   bool stopped = false;
   float totalWeight = 0.0f;
-  float weightLimit = selectedItem == SingleDose ? singleShotLimit / 1000 : doubleShotLimit / 1000;
-  const int weightArraySize = 5;  // Size of the array
-  float weightArray[weightArraySize] = { 0 };
+  float weightOfCoffeeExtractedAfterStop = *getCoeffPtr() / 1000;
+  float weightLimit = *selectedItem == SingleDose ? singleShotLimit / 1000 : doubleShotLimit / 1000;
+  const int weightArraySize = 10;  // Size of the array
+  float weightArray[weightArraySize] = { 0.0f };
 
-  drawMessage("Taring...");
+  showTaringScreen();
   loadcellA.tare();
   loadcellB.tare();
 
   unsigned long startTime = millis();
+  // due to the current the relay needs to switch on and vibrations in the machine there is a certain
+  // level of fault in the measurement once the pump is turned on. This variable helps to correct that
+  float correctionWeight = 0.0f;
+  bool correctionApplied = false;
+  float timeOfCorrection = 1.2f;
+
   digitalWrite(RELAY_PIN, HIGH);
 
   while (true) {
+    // Stop the extraction if the button is pushed
     pushButton.read();
     if (pushButton.wasReleased()) {
       stopped = true;
       break;
     }
 
+    // Stop the extraction if we pass the safety time limit
     finalTime = (float)(millis() - startTime) / 1000;
     if (finalTime > timeLimit) {
       stopped = true;
       break;
     }
 
+    // Calculate the floating average of the extracted coffee weight
     totalWeight = getTotalWeight();
     float weightSum = 0.0f;
     for (int i = weightArraySize - 1; i >= 0; i--) {
@@ -497,11 +630,26 @@ void startExtraction(MenuItem selectedItem) {
     }
     float weightAverage = weightSum / weightArraySize;
 
-    if (weightAverage > weightLimit) {
+    // Vibrations and voltage drop if the relay is switched can cause incorrect weight readings 
+    // at the beginning of extraction. Here we compensate for that by zeroing the weight after 
+    // timeOfCorrection passed.
+    if (finalTime >= timeOfCorrection && !correctionApplied) {
+      correctionApplied = true;
+      correctionWeight = weightAverage;
+    }
+
+    // Finally, stop the extraction once we reach the target weight minus the weight of coffee
+    // that is still poured after the extraction stops (configurable in settings)
+    float correctedWeightAverage = weightAverage - correctionWeight;
+    if (correctedWeightAverage > weightLimit - weightOfCoffeeExtractedAfterStop) {
       break;
     }
 
-    showWeight(weightAverage, (String("Pouring: ") + String(finalTime, 1) + "s").c_str());
+    // Show the current weight of extracted coffee or zero until we get the weight correction
+    showWeight(correctionApplied ? correctedWeightAverage : 0, (String("Pouring: ") + String(finalTime, 1) + "s").c_str());
+
+    // Wait a moment before another loop iteration
+    delay(50);
     yield();
   }
 
@@ -514,7 +662,7 @@ void startExtraction(MenuItem selectedItem) {
   } else {
     writeToMemory(
       STATISTICS_ADDRESS,
-      grindedDosesCount += (selectedItem == SingleDose ? 1 : 2));
+      grindedDosesCount += (*selectedItem == SingleDose ? 1 : 2));
     showScale((String("Done at ") + String(finalTime, 1) + "s").c_str());
   }
 }
